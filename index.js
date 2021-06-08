223c758583b9bac7ebf7d1d86e99a52f74fac579
Miet <miet.vanderheyden@gmail.com>
2021-06-08 11:39:21 -0400

    Created files

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let myLibrary = [];
+
+const addBookButton = document.getElementById("add-book");
+
+function Book (title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+function addBookToLibrary () {
+    // do stuff here
+}
+
+addBookButton.addEventListener('click', () => {
+
+})
